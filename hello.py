53df9b28ec883c670e90936e9fb0942869c4a133
dhanyapatel20 <jjignashapatel16@gmail.com>
2026-03-16 17:40:34 +0000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,5 @@
+print(" hey my name is Dhanya")
+age=10
+print("age=",age)
+s="football"
+print("my favourite sport is",s)
---
+++ user input .py
@@ -0,0 +1,18 @@
+n=input("enter your name =")
+a=int(input("enteryour age ="))
+w=input("enter your weight =")
+print("my name is ",n)
+print("my wieghti is",w)
+print("my name age is",a)
+
+
+
+
+
+
+
+
+
+
+
+
