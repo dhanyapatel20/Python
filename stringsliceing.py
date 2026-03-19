774c49fea29faa8900d49452066637b768fae54f
dhanyapatel20 <jjignashapatel16@gmail.com>
2026-03-19 18:18:20 +0000

    Add files via upload

---
+++ stringsliceing.py
@@ -0,0 +1,7 @@
+s="banana"
+print(s[0::])
+print(s[0:4])
+print(s[3:6])
+print(s[1:6])
+print(s[::-1])
+print(s[0:6:2])
---
+++ typecasting.py
@@ -0,0 +1,15 @@
+x=67
+y=False
+z=40.85678
+print(type(x))
+print(type(y))
+print(type(z))
+print("Aftertypecasting")
+a=float(x)
+print(a)
+b=str(y)
+print(type(b),"=fales")
+c=int(z)
+print(c)
+
+
