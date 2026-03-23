121ecff4a47dfd771716de89a384f0c59ba74ea0
dhanyapatel20 <jjignashapatel16@gmail.com>
2026-03-23 17:51:26 +0000

    Add files via upload

---
+++ average.py
@@ -0,0 +1,9 @@
+a=98
+b=67
+c=57
+d=95
+e=35
+sum=a+b+c+d+e
+print("sumof allthe five numdersis",sum)
+av=sum/5
+print("average=",av)
---
+++ persentage.py
@@ -0,0 +1,9 @@
+print("enteryouer marcks out of 100:")
+m=int(input("maths="))
+e=int(input("eng="))
+s=int(input("sci="))
+h=int(input("his="))
+t=m+e+s+h
+print("markes=",t)
+p=(t/400)*100
+print("percentage:",p)
