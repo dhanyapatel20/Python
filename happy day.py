6cbaab2bd06f8e1c81735efd70a6c9dad8ef8247
dhanyapatel20 <jjignashapatel16@gmail.com>
2026-04-05 16:12:56 +0100

    Add files via upload

---
+++ happy day.py
@@ -0,0 +1,30 @@
+print("i am salesh")
+age=12
+print("my age",age)
+message="happy birthday"
+print("my message is",message)
+
+print("I am lexe")
+age=27
+print("my age is",age)
+message="i love you vidoes i wish you hit 10000000 sud for your birthday HAPPY BIRTHDAY"
+print("my messag is",message)
+
+print("iam dash")
+age=16
+print("my age is",age)
+message="have a good day HAPPY BIRTHDAY"
+print("my message is",message)
+
+print("i am brent")
+age=27
+print("my age is",age)
+message="i like you vidoes th are scare wich i like so thank you and have a fantastic BIRTHDAY"
+print("my messagge is", message)
+      
+print("i am jorden matter")
+age=45
+print("my age is",age)
+message="happy birthday"
+print("my message is",message)
+
