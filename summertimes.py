d2995bca2360dfdc974ed6c9723d723cf347bff6
dhanyapatel20 <jjignashapatel16@gmail.com>
2026-04-06 13:24:56 +0100

    Add files via upload

---
+++ summertimes.py
@@ -0,0 +1,9 @@
+i =int(input("enter a numder: " ))
+if (i< 20):
+    print ("no jacket" )
+    print("i'm in danger")
+else:
+     print("i is >20")
+print("yes jacket" )
+print("i'm not in danger")
+
