1e41d706b7ff0b7b7197b3638d9adcf9c4b147b4
dhanyapatel20 <jjignashapatel16@gmail.com>
2026-04-18 13:10:51 +0100

    Add files via upload

---
+++ age_cheacking_divious.py
@@ -0,0 +1,11 @@
+i=9
+if (i == 20):
+    if (i < 15):
+        print("i is smaller than 20")
+    if (i < 10):
+        print("i is older  than 10 ")
+    else:
+        print(" i is oleder then 10 and yonger then 20")
+
+        
+              
