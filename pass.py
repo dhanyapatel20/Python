b96b2556d27f35205ad87aa48e5d19b61d3279e0
dhanyapatel20 <jjignashapatel16@gmail.com>
2026-06-27 19:48:43 +0100

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,16 @@
+for x in range(10):
+    if x % 20==0:
+        print("twist")
+    
+    elif x % 2==0:
+        pass
+    elif x % 5==0:
+        print("fizzz")
+
+
+    elif x % 3==0:
+        print("duzz")
+    
+    else:
+        print(x)
+        
