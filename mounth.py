1bb54669f28ed783f3eb1865456ea48e037bcea2
dhanyapatel20 <jjignashapatel16@gmail.com>
2026-07-20 17:06:36 +0100

    Add monthly birthday print statements
    Added print statements for each month with associated names and birthdays.

---
+++ mounth.py
@@ -0,0 +1,35 @@
+print("this is january")
+print("i am raj and it is my birthday")
+
+print("this is february")
+print("i am mohit and it is my birthday")
+
+print("this is march")
+print("i am arjun and it is my birthday")
+
+print("this is april")
+print("i am piyush and it is my birthday")
+
+print("this is may")
+print("i am ankit and it is my birthday")
+
+print("this is june")
+print("i am shubham and it is my birthday")
+
+print("this is july")
+print("i am ansh and it is my birthday")
+
+print("this is august")
+print("i am ansh and it is my birthday")
+
+print("this is september")
+print("i am ansh and it is my birthday")
+
+print("this is october")
+print("i am dhanya and it is my birthday")
+
+print("this is november")
+print("i am ansh and it is my birthday")
+
+print("this is december")
+print("i am ansh and it is my birthday")
