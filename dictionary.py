289cdcbe075cd6274c2be4e5c23dff1a3f5f88bf
dhanyapatel20 <jjignashapatel16@gmail.com>
2026-08-10 16:39:25 +0100

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,27 @@
+print ("my name is raj")
+print ("i am 12 years old")
+print ("i am from india")
+print ("i am a student")
+print ("my most loved subject is maths")
+
+print ("my name is ver")
+print ("i am 11 years old")
+print ("i am from india")
+print ("i am a student")
+print ("my most loved subject is science")
+
+
+
+print ("my name is henil")
+print ("i am 11 years old")
+print ("i am from india")
+print ("i am a student")
+print ("my most loved subject is history")
+
+
+
+print ("my name is jiya")
+print ("i am 13 years old")
+print ("i am from india")
+print ("i am a student")
+print ("my most loved subject is english")
