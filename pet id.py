5380d1bd2cc7e1e12d20ca4afa2a80c9738fe525
dhanyapatel20 <jjignashapatel16@gmail.com>
2026-08-24 21:22:36 +0100

    Add files via upload

---
+++ pet id.py
@@ -0,0 +1,11 @@
+class Parrot:
+    species = "bird"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Dog:
+    species = "mammal"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
